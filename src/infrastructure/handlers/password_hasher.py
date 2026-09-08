857036839e4ba26325fbba9b46decf5eb8e59ada
Patrick <leme.patrick.contato@hotmail.com>
2026-09-07 22:28:24 -0300

    added to is_bcrypt_hash to PasswordHasher

--- src/infrastructure/handlers/password_hasher.py
+++ src/infrastructure/handlers/password_hasher.py
@@ -1,9 +1,17 @@
+import re
 import bcrypt
-
 
 class PasswordHasher():
     @staticmethod
     def hash_password(password: str) -> str:
         """Hash password using bcrypt"""
         salt = bcrypt.gensalt()
+        
         return bcrypt.hashpw(password.encode('utf-8'), salt).decode('utf-8')
+    
+    @staticmethod
+    def is_bcrypt_hash(password: str) -> bool:
+        """Check if string matches bcrypt format"""
+        bcrypt_pattern = r'^\$2[aby]\$\d{2}\$[./A-Za-z0-9]{53}$'
+
+        return bool(re.match(bcrypt_pattern, password))
